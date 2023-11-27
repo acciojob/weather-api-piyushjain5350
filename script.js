1c6b70284a746d8aa0f4106a3085c46e34244d31
Piyush Jain <126599802+piyushjain5350@users.noreply.github.com>
2023-11-27 23:46:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function get(){
+	const url=`https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=e467712b257e418838be97cc881a71de`;
+	fetch(url)
+	.then((response)=>response.json())
+	.then(data=>{
+		const res=document.getElementById("weatherData");
+		res.innerHTML=`Current weather in London: ${data.weather[0].main}`;
+	})
+}
+const button=document.getElementById("btn");
+button.addEventListener("click",get);
